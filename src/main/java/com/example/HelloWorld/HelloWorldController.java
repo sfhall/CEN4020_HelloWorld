b4cd56efde8b896da78218e7af627aed0ed31f4f
Sarah <sfhall22@gmail.com>
2021-01-28 18:34:57 -0500

    init push

---
+++ src/main/java/com/example/HelloWorld/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.example.HelloWorld;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String index()
+    {
+        return "Hello World";
+    }
+}
